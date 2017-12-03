27e7d3628ad1b2e0539b75813f0ba93df59e59df
shadyrifaii <33735667+shadyrifaii@users.noreply.github.com>
2017-12-03 14:26:57 +0200

    Yeoman

---
+++ src/app/scripts/spExamsTabConfig.ts
@@ -0,0 +1,37 @@
+import {TeamsTheme} from './theme';
+
+/**
+ * Implementation of SPExams Tab configuration page
+ */
+export class spExamsTabConfig {
+    constructor() {
+        microsoftTeams.initialize();
+
+        microsoftTeams.getContext((context:microsoftTeams.Context) => {
+            TeamsTheme.fix(context);
+            let val = <HTMLInputElement>document.getElementById("data");
+            if (context.entityId) {
+                val.value = context.entityId;
+            }
+            this.setValidityState(true);
+        });
+
+        microsoftTeams.settings.registerOnSaveHandler((saveEvent: microsoftTeams.settings.SaveEvent) => {
+
+            let val = <HTMLInputElement>document.getElementById("data");
+			// Calculate host dynamically to enable local debugging
+			let host = "https://" + window.location.host;
+            microsoftTeams.settings.setSettings({
+                contentUrl: host + "/spExamsTabTab.html?data=",
+                suggestedDisplayName: 'SPExams Tab',
+                removeUrl: host + "/spExamsTabRemove.html",
+				entityId: val.value
+            });
+
+            saveEvent.notifySuccess();
+        });
+    }
+    public setValidityState(val: boolean) {
+        microsoftTeams.settings.setValidityState(val);
+    }
+}
